22b2ab5be0990926c833c41078bf537fce22fef2
moythry119 <moythry.samia119@gmail.com>
2023-07-20 10:14:17 +0600

    first commit

---
+++ PostGresExamples/src/main/java/com/jpa/PostGresExamples/App.java
@@ -0,0 +1,13 @@
+package com.jpa.PostGresExamples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
